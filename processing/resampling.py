d7e993c66cbeecf3cdf8d741322088f1899b900f
Quincy Cambrel <qmcambrel@gmail.com>
2025-11-03 08:12:11 -0500

    added test for dimensionality

--- processing/resampling.py
+++ processing/resampling.py
@@ -2,4 +2,7 @@ import numpy as np
 from sunpy.image import resample as rs
 
 def resample(data: np.ndarray, shape: tuple, center=True):
-    return rs(data, shape, center=center)
+    """
+    Resamples an image to the given shape.
+    """
+    return rs.resample(data, shape, center=center)
