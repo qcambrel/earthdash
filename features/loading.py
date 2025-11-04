52c7810489c0f73fe734ef90c8fe30b652177402
Quincy Cambrel <qmcambrel@gmail.com>
2025-11-03 19:56:56 -0500

    set up baseline feature loading

--- features/loading.py
+++ features/loading.py
@@ -1,24 +1,47 @@
 import os
 import boto3
+import zipfile
 from PIL import Image
+from utils import constants
+from utils.schemas import (
+    BackgroundContext, CoastlineContext, BorderContext
+)
+from features.caching import (
+    cache_background, cache_coastlines, cache_borders
+)
 
-def load_background():
+def load_background(cache_dir: str, context: BackgroundContext) -> str:
     """
     Loads pre-rendered background image for the given dataset.
     """
-    pass
+    fname = f"{context.tag}-{context.background_name}.png"
+    fpath = os.path.join(cache_dir, fname)
+    if os.path.exists(cache_dir) and os.path.exists(fpath):
+        return fpath
+    else:
+        return cache_background(cache_dir, context)
 
-def load_coastlines():
+def load_coastlines(cache_dir: str, temp_dir: str, context: CoastlineContext) -> str:
     """
     Loads pre-rendered coastlines for the given dataset.
     """
-    pass
+    fname = f"{context.tag}-coastlines.png"
+    fpath = os.path.join(cache_dir, fname)
+    if os.path.exists(cache_dir) and os.path.exists(fpath):
+        return fpath
+    else:
+        return cache_coastlines(cache_dir, temp_dir, context)
 
-def load_borders():
+def load_borders(cache_dir: str, temp_dir: str, context: BorderContext):
     """
     Loads pre-rendered state and country borders for the given dataset.
     """
-    pass
+    fname = f"{context.tag}-borders.png"
+    fpath = os.path.join(cache_dir, fname)
+    if os.path.exists(cache_dir) and os.path.exists(fpath):
+        return fpath
+    else:
+        return cache_borders(cache_dir, temp_dir, context)
 
 def load_roads():
     """
