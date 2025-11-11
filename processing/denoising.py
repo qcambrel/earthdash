c73dcd5d5c63f411474562190a863542d760b1a4
Quincy Cambrel <qmcambrel@gmail.com>
2025-11-11 11:05:30 -0500

    implemented simple nonlocal means

--- processing/denoising.py
+++ processing/denoising.py
@@ -5,8 +5,17 @@ from skimage.restoration import denoise_nl_means, estimate_sigma
 def nonlocal_means(data: np.ndarray, fast: bool = False) -> np.ndarray:
     """
     Applies nonlocal means denoising to a 2D array.
+
+    Args:
+        data (np.ndarray): The image to denoise
+        fast (bool, optional): Whether to use fast mode. Defaults to False.
+
+    Returns:
+        np.ndarray: The denoised image
     """
-    pass
+    sigma = np.mean(estimate_sigma(data, channel_axis=-1))
+    denoised = denoise_nl_means(data, sigma=sigma, fast_mode=fast)
+    return denoised
 
 def bandpass_filter(data: np.ndarray, low: float, high: float, context: BandpassContext) -> np.ndarray:
     """
